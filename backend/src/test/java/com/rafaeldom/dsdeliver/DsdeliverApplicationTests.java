07473a518766429716661f831e28aa57d9ac96a9
RafaDom10 <r-domingues@outlook.com>
2021-01-04 13:20:56 -0300

    Project created

---
+++ backend/src/test/java/com/rafaeldom/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaeldom.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

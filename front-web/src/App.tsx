bce7a24adca99d8a8f5529b71fb6a5e1e5f6349c
RafaDom10 <r-domingues@outlook.com>
2021-01-08 16:01:34 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './App.css';
 
 import Routes from './routes';
 
 function App() {
-  return (    
-     <Routes />    
+  return (
+    <>
+     <Routes />
+     <ToastContainer />
+    </>
   )
 }
 
